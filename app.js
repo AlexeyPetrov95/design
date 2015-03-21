a5c3e1f54c52188d16415e15da7deda5c5669b65
Алексей <aleksej@iMac-Aleksej.local>
2015-03-22 01:09:05 +0300

    Добавил сессии

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var admin = require('./routes/admin');
@@ -20,6 +21,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session ({
+    secret: 'awesomeServer',
+    key: 'sid',
+    cookie: { httpOnly: true, maxAge: null }
+}));
+        
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
