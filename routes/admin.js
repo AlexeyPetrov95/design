30c489086b818217240f173bf6a525f5a90a42a7
Алексей <aleksej@iMac-Aleksej.local>
2015-03-22 01:10:58 +0300

    Добавил авторизацию

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../system/system.json');
 
 router.get('/admin', function(req, res){
     res.render('admin', {title: "admin"});  
 })
 
 router.post('/admin', function(req, res){
-    console.log("1232");
-    res.send();
+    for (var i in data){
+        if (data[i].login == req.body.login && data[i].password == req.body.password){
+             req.session.authorized = true;
+        }
+    }
+    
+    if (req.session.authorized){
+        res.send(200);   
+    }else {
+        res.redirect('/admin');   
+    }
+    
 })
 
 module.exports = router;
