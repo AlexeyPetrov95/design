6b81dd725083805e086e5c049a993db9876ded35
Алексей <aleksej@iMac-Aleksej.local>
2015-03-22 12:44:06 +0300

    рефакторинг

--- routes/admin.js
+++ routes/admin.js
@@ -2,23 +2,34 @@ var express = require('express');
 var router = express.Router();
 var data = require('../system/system.json');
 
-router.get('/admin', function(req, res){
-    res.render('admin', {title: "admin"});  
+router.get('/login', function(req, res){
+    if (req.session.authorized){
+        res.redirect('/admin')   
+    }else {
+        res.render('login', {title: 'login'});     
+    }
 })
 
-router.post('/admin', function(req, res){
+router.post('/login', function(req, res){
     for (var i in data){
         if (data[i].login == req.body.login && data[i].password == req.body.password){
              req.session.authorized = true;
         }
     }
-    
     if (req.session.authorized){
-        res.send(200);   
-    }else {
-        res.redirect('/admin');   
+        res.redirect('/admin'); 
+    } else {
+        res.redirect('/login');   
     }
     
+})
+
+router.get('/admin', function(req, res){
+    if (req.session.authorized){
+        res.render('admin', {title: '123'});   
+    }else {
+        res.redirect('/login');   
+    }
 })
 
 module.exports = router;
