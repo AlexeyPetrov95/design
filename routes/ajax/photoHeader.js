73ab876ebe8f6787ce31a5bf42c9122c709914fb
Алексей <aleksej@iMac-Aleksej.local>
2015-03-23 18:57:18 +0300

    Добавил пути для ajax

---
+++ routes/ajax/photoHeader.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+router.get('/getPhotoHeader', function(req, res){
+    fs.readdir('./public/images/main', function(err, files){
+        for (var i in files){
+            files[i] = '/images/main/' + files[i];
+        }
+        res.send(files);
+    })
+});
+
+module.exports = router;
