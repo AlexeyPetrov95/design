4b4fdf0091cc5510404b6b758512a4a49271d5c8
Alexey Petrov <alexeyPetrov@MacBook-Pro-Alexey.local>
2015-03-29 11:50:11 +0300

    разработка главной страницы

---
+++ public/javascripts/animation.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	
+	$('.animation').hide();
+	$(".bottom").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'), top = $(id).offset().top - 48;
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+	
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > 200){
+			$('.navigation').css('background', 'rgba(0,0,0,0.9)');
+		} else {
+			$('.navigation').css('background', 'rgba(0,0,0,0.7)');
+		}
+	})
+	
+	$('.left').hover(function(){
+		$(this).find('.projectTitle').hide();
+		$(this).find('.animation').slideDown(100);
+	}, function(){
+		$(this).find('.projectTitle').show();
+		$(this).find('.animation').hide();
+	});
+	
+	$('.right').hover(function(){
+		$(this).find('.projectTitle').hide();
+		$(this).find('.animation').slideDown(100);
+		
+	}, function(){
+		$(this).find('.projectTitle').show();
+		$(this).find('.animation').hide();
+	});
+	
+});
